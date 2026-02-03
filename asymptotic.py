53bbfd6eaed44d65496533d6f2eba1f03fd4302d
sarahsinha123 <sinhasarah47@gmail.com>
2026-02-03 16:24:36 +0530

    py py

---
+++ asymptotic.py
@@ -0,0 +1,8 @@
+def printnumber(n):
+    iteration=0
+    print("the number entered by user is",n)
+    iteration+=1
+    print("total iterations is",iteration,"\n")
+printnumber(10)
+printnumber(20)
+print("\n with any 'n' the time taken wont change")
